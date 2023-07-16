f638b95e70cf48fad16119843c4bfce4ef473e92
shikha <shikha@gmail.com>
2023-07-16 16:06:07 +0530

    contacts are being added

--- lib/view/screens/contact_page_screen.dart
+++ lib/view/screens/contact_page_screen.dart
@@ -7,38 +7,73 @@ import 'package:whatsapp_clone/app_resources/app_colors.dart';
 import 'package:whatsapp_clone/app_resources/app_dimension.dart';
 import 'package:whatsapp_clone/app_resources/app_style.dart';
 import 'package:whatsapp_clone/controller/contact_controller.dart';
+import 'package:whatsapp_clone/model/user_model.dart';
 
 class ContactPageScreen extends StatelessWidget {
-   ContactPageScreen({super.key});
+  // tumhari bat ho rahi?kyaa
+  ContactPageScreen({super.key});
   ContactController contactController = Get.put(ContactController());
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: AppColor.whatsAppbarColor,
-        leading: BackButton(),
-        title: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text("Select contact", style: AppStyles.fontwhite,),
-            SizedBox(height: AppDimension.px3,),
-            Text("5 contacts", style: TextStyle(fontSize: AppDimension.px12),),
+        appBar: AppBar(
+          backgroundColor: AppColor.whatsAppbarColor,
+          leading: BackButton(),
+          title: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Select contact",
+                style: AppStyles.fontwhite,
+              ),
+              SizedBox(
+                height: AppDimension.px3,
+              ),
+              Text(
+                "5 contacts",
+                style: TextStyle(fontSize: AppDimension.px12),
+              ),
+            ],
+          ),
+          actions: [
+            IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+            IconButton(onPressed: () {}, icon: Icon(Icons.more_vert)),
           ],
-        ),
-        actions: [
-          IconButton(onPressed: (){}, icon: Icon(Icons.search)),
-          IconButton(onPressed: (){}, icon: Icon(Icons.more_vert)),
-        ],
-      ),
-      body: ListView.builder(
-        itemCount: 5,
-        itemBuilder: (context, index){
-        return Card(
-          child: Text("abcd "),
-        );
-      })
-    );
-    
+        ), //tum apna krdo expanded me kyu wrap kiyaadono kyunnnlistview ka useok/hnn
+        body: Obx(
+          () => contactController.contact.isEmpty
+              ? SizedBox()
+              : ListView(
+                  children: [
+                    Text("Contacs on Whatsapp"),
+                    Expanded(
+                      child: ListView.builder(
+                        physics: NeverScrollableScrollPhysics(),
+                        shrinkWrap: true,
+                        itemCount: contactController.contact[0].length,
+                        itemBuilder: (context, index) {
+                          return Text(
+                            contactController.contact[0][index].username,
+                          );
+                        },
+                      ),
+                    ),
+                    Text("Contacs"),
+                    Expanded(
+                      child: ListView.builder(
+                         physics: NeverScrollableScrollPhysics(),
+                        shrinkWrap: true,
+                        itemCount: contactController.contact[1].length,
+                        itemBuilder: (context, index) {
+                          return Text(
+                            contactController.contact[1][index].username,
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+        ));
   }
 }
